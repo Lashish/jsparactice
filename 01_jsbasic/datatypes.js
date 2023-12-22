8945a0d94e941823f5924e302531f738eafd92e9
ashish kumar <103443907+Lashish@users.noreply.github.com>
2023-12-22 09:46:59 +0000

    save new file

---
+++ 01_jsbasic/datatypes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// datatypes
+// Number=> 2^53
+// bigint
+// String
+// Boolean => 
+// null => Standalone value
+// undefined =>
+// symbol => unique
+
+console.log(typeof null);// object
+console.log(typeof undefined);// undefined
+
+// object
---
+++ 01_jsbasic/variable.js
@@ -0,0 +1,8 @@
+const myId = 1243;
+let myName = 'ashishkumar';
+var myCity = "Etawah";
+newcity = 'Ballia';
+myCity = 'freeCity';
+// prefer not to use var
+
+console.table([myId,myName, myCity, newcity, myCity ]);
